8b6ff551be2a023e4cb87430035c294bbeef6e8d
Javi Carretero <fracargar@gmail.com>
2016-10-09 15:44:48 +0000

    Add solutions to HackerRank Algorithms challenges: - A Very Big Sum (Java)

---
+++ src/hackerrank/java/AVeryBigSum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class AVeryBigSum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long sum = IntStream.range(0, in.nextInt())
+                .mapToLong(i -> in.nextLong())
+                .sum();
+        System.out.println(sum);
+    }
+}
