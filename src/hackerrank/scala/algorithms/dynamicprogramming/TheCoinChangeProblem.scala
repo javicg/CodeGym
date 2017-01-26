3a44c3070bf8625a28fcef0a0f96ac64c32f8d7a
Javi Carretero <fracargar@gmail.com>
2017-01-26 19:33:57 +0000

    Solution for The Coin Change Problem, using Dynamic Programming

--- src/hackerrank/scala/algorithms/dynamicprogramming/TheCoinChangeProblem.scala
+++ src/hackerrank/scala/algorithms/dynamicprogramming/TheCoinChangeProblem.scala
@@ -6,31 +6,65 @@ object TheCoinChangeProblem extends App {
   val change = in.nextLine().split("\\s")(0).toInt
   val coins = in.nextLine().split("\\s").map(_.toInt)
 
-  val memo = Array.ofDim[Int](250, 50)
-  val memoSet = Array.ofDim[Boolean](250, 50)
-  println(computeChangeCombinations(change, coins))
+  Dynamic.solve(change, coins)
 
-  def computeChangeCombinations(qty: Int, coins: Array[Int]): Int = {
-    if (qty == 0 || coins.isEmpty) {
-      0
-    } else {
-      val coin = coins.head
-      if (!memoSet(qty)(coin)) {
-        memo(qty)(coin) = computeChangeCombinationsWithCoin(qty, coins, coin)
-        memoSet(qty)(coin) = true
+  object Dynamic {
+    def solve(change: Int, coins: Array[Int]): Unit = {
+      val D = Array.ofDim[Long](change + 1, coins.length + 1)
+
+      for (q <- 1 to change) {
+        for (i <- 1 to coins.length) {
+          val c = coins(i-1)
+          D(q)(i) = D(q)(i-1) + computeAddition(q, i, c)
+        }
       }
-      memo(qty)(coin)
+
+      println(D(change)(coins.length))
+
+      def computeAddition(q: Int, i: Int, c: Int): Long = {
+        if (q == c)
+          1
+        else if (c < q)
+          D(q - c)(i)
+        else
+          0
+      }
     }
   }
 
-  def computeChangeCombinationsWithCoin(qty: Int, coins: Array[Int], coin: Int): Int = {
-    val combinationsFromTail = computeChangeCombinations(qty, coins.tail)
-    if (coin == qty) {
-      1 + combinationsFromTail
-    } else if (coin < qty) {
-      computeChangeCombinations(qty - coin, coins) + combinationsFromTail
-    } else {
-      combinationsFromTail
+  object RecursiveWithMemo {
+
+    def solve(change: Int, coins: Array[Int]): Unit = {
+      val memo = Array.ofDim[Int](250, 50)
+      val memoSet = Array.ofDim[Boolean](250, 50)
+      println(computeChangeCombinations(change, coins, memo, memoSet))
+    }
+
+    private def computeChangeCombinations(qty: Int, coins: Array[Int],
+                                          memo: Array[Array[Int]], memoSet: Array[Array[Boolean]]): Int = {
+      if (qty == 0 || coins.isEmpty) {
+        0
+      } else {
+        val coin = coins.head
+        if (!memoSet(qty)(coin)) {
+          memo(qty)(coin) = computeChangeCombinationsWithCoin(qty, coins, coin, memo, memoSet)
+          memoSet(qty)(coin) = true
+        }
+        memo(qty)(coin)
+      }
+    }
+
+    private def computeChangeCombinationsWithCoin(qty: Int, coins: Array[Int], coin: Int,
+                                                  memo: Array[Array[Int]], memoSet: Array[Array[Boolean]]): Int = {
+      val combinationsFromTail = computeChangeCombinations(qty, coins.tail, memo, memoSet)
+      if (coin == qty) {
+        1 + combinationsFromTail
+      } else if (coin < qty) {
+        computeChangeCombinations(qty - coin, coins, memo, memoSet) + combinationsFromTail
+      } else {
+        combinationsFromTail
+      }
     }
   }
+
 }
