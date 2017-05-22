85a64ffbdafef59a108e152d19585b7904b1e06d
Javi Carretero <fracargar@gmail.com>
2017-05-22 22:20:15 +0100

    Attempt to solve Journey to the Moon (HackerRank). All tests finish on time, but a few with the wrong solution

--- src/hackerrank/scala/algorithms/graphs/JourneyToTheMoon.scala
+++ src/hackerrank/scala/algorithms/graphs/JourneyToTheMoon.scala
@@ -1,9 +1,7 @@
 package algorithms.graphs
 
 // TODO Review (wrong solution)
-// - Runtime error on some cases (memory issues?)
-// - Wrong answer on 1 case
-// - Timeout in the majority (pre-computing all combinations is inefficient!)
+// - Wrong answer on many cases. Review algorithm!
 object JourneyToTheMoon extends App {
   val in = new java.util.Scanner(System.in)
 
@@ -15,37 +13,43 @@ object JourneyToTheMoon extends App {
 
   (1 to p).foreach(_ => {
     val pair = in.nextLine().split("\\s").map(_.toInt)
-    graph.group(pair(0), pair(1))
+    graph.connect(pair(0), pair(1))
   })
 
   println(graph.countUnconnectedPairs())
 
   class Graph(n: Int) {
-    private var connected: Map[Int, Set[Int]] = Map.empty
-    private var unconnected: Map[Int, Set[Int]] = Map.empty
+    private var adj: Map[Int, Set[Int]] = Map.empty
+    (0 until n).foreach(el => adj += el -> Set.empty)
 
-    for (i <- 0 until n-1) {
-      unconnected += i -> (i+1 until n).toSet
+    def connect(i: Int, j: Int): Unit = {
+      pair(i, j)
+      adj(i).foreach(a => pair(j, a))
+      adj(j).foreach(a => pair(i, a))
     }
 
-    def group(i: Int, j: Int): Unit = {
-      if (i > j) {
-        group(j, i)
-      } else {
-        val neighbours = connected.get(i)
-        if (neighbours.isEmpty) {
-          connected += i -> Set(j)
-          unconnected += i -> unconnected(i).filterNot(el => el == j)
+    private def pair(i: Int, j: Int): Unit = {
+      addAdj(i, j)
+      addAdj(j, i)
+    }
+
+    private def addAdj(i: Int, j: Int): Unit = {
+      if (i != j) {
+        val adjs = adj.get(i)
+        if (adjs.isEmpty) {
+          adj += i -> Set(j)
         } else {
-          neighbours.get.foreach(el => group(el, j))
-          connected += i -> (connected(i) + j)
-          unconnected += i -> unconnected(i).filterNot(el => el == j)
+          adj += i -> (adjs.get + j)
         }
       }
     }
 
     def countUnconnectedPairs(): Long = {
-      unconnected.values.map(s => s.size.toLong).sum
+      var sum: Long = 0
+      for (k <- adj.keys) {
+        sum += (n - 1) - adj(k).size
+      }
+      sum / 2
     }
   }
 }
