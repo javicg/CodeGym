52262e29c496ebeff9f7906f8053be26a057a3a7
Javi Carretero <fracargar@gmail.com>
2017-05-26 22:30:03 +0100

    Submit solution for Journey to the Moon, from HackerRank (correct algorithm this time)

--- src/hackerrank/scala/algorithms/graphs/JourneyToTheMoon.scala
+++ src/hackerrank/scala/algorithms/graphs/JourneyToTheMoon.scala
@@ -1,7 +1,5 @@
 package algorithms.graphs
 
-// TODO Review (wrong solution)
-// - Wrong answer on many cases. Review algorithm!
 object JourneyToTheMoon extends App {
   val in = new java.util.Scanner(System.in)
 
@@ -9,47 +7,43 @@ object JourneyToTheMoon extends App {
   val n = firstLine(0)
   val p = firstLine(1)
 
-  val graph = new Graph(n)
+  val graph = new Cluster(n)
 
   (1 to p).foreach(_ => {
     val pair = in.nextLine().split("\\s").map(_.toInt)
     graph.connect(pair(0), pair(1))
   })
 
-  println(graph.countUnconnectedPairs())
+  println(graph.countMixedPairs())
 
-  class Graph(n: Int) {
-    private var adj: Map[Int, Set[Int]] = Map.empty
-    (0 until n).foreach(el => adj += el -> Set.empty)
+  class Cluster(n: Int) {
+    private val countries: Array[Int] = (0 until n).toArray
 
     def connect(i: Int, j: Int): Unit = {
-      pair(i, j)
-      adj(i).foreach(a => pair(j, a))
-      adj(j).foreach(a => pair(i, a))
+      if (i > j) {
+        connect(j, i)
+      } else if (i < j) {
+        val countryI = countries(i)
+        val countryJ = countries(j)
+        countries.indices
+          .filter(index => countries(index) == countryJ)
+          .foreach(index => countries(index) = countryI)
+      }
     }
 
-    private def pair(i: Int, j: Int): Unit = {
-      addAdj(i, j)
-      addAdj(j, i)
-    }
-
-    private def addAdj(i: Int, j: Int): Unit = {
-      if (i != j) {
-        val adjs = adj.get(i)
-        if (adjs.isEmpty) {
-          adj += i -> Set(j)
-        } else {
-          adj += i -> (adjs.get + j)
+    def countMixedPairs(): Long = {
+      var sum: Long = 0
+      val groups = groupCountries()
+      for (i <- groups.indices) {
+        for (j <- i+1 until groups.length) {
+          sum += groups(i) * groups(j)
         }
       }
+      sum
     }
 
-    def countUnconnectedPairs(): Long = {
-      var sum: Long = 0
-      for (k <- adj.keys) {
-        sum += (n - 1) - adj(k).size
-      }
-      sum / 2
+    private def groupCountries(): Array[Int] = {
+      countries.groupBy(x => x).mapValues(_.length).values.toArray
     }
   }
 }
