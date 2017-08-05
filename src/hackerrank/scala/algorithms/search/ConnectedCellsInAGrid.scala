106a93b53747cdf3afaf7c0bdf3c61372ad70846
Javi Carretero <fracargar@gmail.com>
2017-08-05 12:24:40 +0100

    Improve solution (get rid of warnings after upgrade to Scala 2.12)

--- src/hackerrank/scala/algorithms/search/ConnectedCellsInAGrid.scala
+++ src/hackerrank/scala/algorithms/search/ConnectedCellsInAGrid.scala
@@ -11,14 +11,14 @@ object ConnectedCellsInAGrid extends App {
     matrix(i) = in.nextLine().split("\\s").map(_.toInt)
   }
 
-  val filled = new scala.collection.mutable.Stack[Cell]()
-  val nonFilled = new scala.collection.mutable.Stack[Cell]()
+  var filled = List.empty[Cell]
+  var nonFilled = List.empty[Cell]
 
   val startingCell = Cell(0, 0)
   if (isFilled(startingCell)) {
-    filled.push(startingCell)
+    filled = startingCell +: filled
   } else {
-    nonFilled.push(startingCell)
+    nonFilled = startingCell +: nonFilled
   }
 
   var maxCountInRegion = 0
@@ -26,7 +26,8 @@ object ConnectedCellsInAGrid extends App {
   var visited = Set.empty[Cell]
   while (filled.nonEmpty || nonFilled.nonEmpty) {
     if (filled.nonEmpty) {
-      val cell = filled.pop
+      val cell = filled.head
+      filled = filled.tail
       if (!visited.contains(cell)) {
         currentCount += 1
         visit(cell)
@@ -36,7 +37,8 @@ object ConnectedCellsInAGrid extends App {
         maxCountInRegion = currentCount
       }
       currentCount = 0
-      val cell = nonFilled.pop
+      val cell = nonFilled.head
+      nonFilled = nonFilled.tail
       if (!visited.contains(cell)) {
         visit(cell)
       }
@@ -48,12 +50,12 @@ object ConnectedCellsInAGrid extends App {
   private def visit(cell: Cell): Unit = {
     visited += cell
     val neighbours = allNeighbours(cell).filter(insideMatrix).partition(isFilled)
-    neighbours._1.foreach(filled.push)
-    neighbours._2.foreach(nonFilled.push)
+    filled = neighbours._1 ++ filled
+    nonFilled = neighbours._2 ++ nonFilled
   }
 
-  private def allNeighbours(cell: Cell): Set[Cell] = {
-    Set(
+  private def allNeighbours(cell: Cell): List[Cell] = {
+    List(
       Cell(cell.i - 1, cell.j),
       Cell(cell.i - 1, cell.j + 1),
       Cell(cell.i, cell.j + 1),
