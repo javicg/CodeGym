3ab17f08a7fefbfa9e8d8d052981a6c420f4435e
Javi Carretero <fracargar@gmail.com>
2017-09-26 20:02:07 +0100

    Optimise GemStones (memory, runtime on big numbers)

--- src/hackerrank/java/algorithms/GemStones.java
+++ src/hackerrank/java/algorithms/GemStones.java
@@ -12,23 +12,49 @@ public class GemStones {
             rocks[i] = in.nextLine();
         }
 
-        Map<Character, Integer> cache = new HashMap<>();
-        for (int i = 0; i < rocks.length; i++) {
-            String rock = rocks[i];
-            Set<Character> visited = new HashSet<>();
-            for (int j = 0; j < rock.length(); j++) {
-                char c = rock.charAt(j);
-                if (!visited.contains(c)) {
-                    Integer count = cache.get(c);
-                    if (count == null) {
-                        count = 0;
+        System.out.println(Version2.calculateGemstones(rocks));
+    }
+
+    private static class Version2 {
+        private static final char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        static long calculateGemstones(String[] rocks) {
+            int counter = 0;
+            for (char c : ALPHABET) {
+                boolean isGemstone = true;
+                for (String rock : rocks) {
+                    if (!rock.contains("" + c)) {
+                        isGemstone = false;
+                        break;
                     }
-                    visited.add(c);
-                    cache.put(c, count + 1);
+                }
+                if (isGemstone) {
+                    counter++;
                 }
             }
+            return counter;
         }
+    }
 
-        System.out.println(cache.values().stream().filter(c -> c == n).count());
+    private static class Version1 {
+        static long calculateGemstones(String[] rocks, int n) {
+            Map<Character, Integer> cache = new HashMap<>();
+            for (String rock : rocks) {
+                Set<Character> visited = new HashSet<>();
+                for (int j = 0; j < rock.length(); j++) {
+                    char c = rock.charAt(j);
+                    if (!visited.contains(c)) {
+                        Integer count = cache.get(c);
+                        if (count == null) {
+                            count = 0;
+                        }
+                        visited.add(c);
+                        cache.put(c, count + 1);
+                    }
+                }
+            }
+
+            return cache.values().stream().filter(c -> c == n).count();
+        }
     }
 }
