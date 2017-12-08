0385680e01b02edd5f0cd1262a5c5c5762ff4476
Javi Carretero Garcia <javi.carretero@ocado.com>
2017-12-08 08:40:00 +0000

    Add solution for CorruptionChecksum part 2
    We can optimise the runtime but this version works.

--- src/advent/scala/CorruptionChecksum.scala
+++ src/advent/scala/CorruptionChecksum.scala
@@ -1,6 +1,6 @@
 import scala.io.Source
 
-object CorruptionChecksum extends App {
+object CorruptionChecksum_Part1 extends App {
   var sum = 0
   for (line <- Source.fromFile("src/advent/scala/CorruptionChecksum.txt").getLines) {
     var min, max = -1
@@ -16,4 +16,23 @@ object CorruptionChecksum extends App {
     sum += max - min
   }
   println(sum)
+}
+
+object CorruptionChecksum_Part2 extends App {
+  var sum = 0
+  for (line <- Source.fromFile("src/advent/scala/CorruptionChecksum.txt").getLines) {
+    val lineValues = line.split("\\s")
+    for (value1 <- lineValues; value2 <- lineValues) {
+      if (value1 != value2) {
+        val number1 = value1.toInt
+        val number2 = value2.toInt
+        if (number1 % number2 == 0) {
+          sum += number1 / number2
+        } else if (number2 % number1 == 0) {
+          sum += number2 / number1
+        }
+      }
+    }
+  }
+  println(sum / 2)
 }
